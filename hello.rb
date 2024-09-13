c874098d10713b44d51d8a8ac035f67b9280ea2b
unknown <danilreshetka@gmail.com>
2024-09-13 16:16:05 +0300

    added question to user

--- hello.rb
+++ hello.rb
@@ -1,1 +1,14 @@
-puts "Hello World!"
+puts "Привет, " + ARGV[0]
+puts "Введите ваш любимый язык программирования: "
+user_result = $stdin.gets.chomp
+if user_result == "ruby"
+    puts "ТЫ ПОДЛИЗА!!!"
+else
+    case user_result
+        when "c++" then puts "Не модно, не стильно, не молодёжно"
+        when "java" then puts "Маинкрафт сделан на джаве" 
+        when "assembler" then puts "Соболезную..."
+        when "python" then puts "Стильно, модно, молодёжно"    
+    end
+    puts "В принципе не важно, всё равно скоро будет ruby."
+end
