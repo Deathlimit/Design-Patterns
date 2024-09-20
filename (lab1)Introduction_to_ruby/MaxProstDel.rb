e85ef68d66a7e4869ad69cff7721564163d07ef8
unknown <danilreshetka@gmail.com>
2024-09-20 16:46:12 +0300

    added method which find max prost del. task 4 n 1

---
+++ (lab1)Introduction_to_ruby/MaxProstDel.rb
@@ -0,0 +1,30 @@
+def is_prostoe?(element)
+  count_delitel = 0
+  for i in (2..element-1)
+    if element % i == 0 
+      count_delitel+=1
+    end
+  end
+  if (count_delitel==0) 
+    return true
+  else 
+    return false
+  end
+end
+
+def max_prostoi_del(element)
+  max_del = 1
+  for i in (2..element)
+    if (element % i == 0) and (is_prostoe?(i))
+      max_del=i
+     end
+   end
+  if (max_del!=1)
+    return max_del
+  else
+    return "Такого делителя нет"
+  end
+end 
+
+number = 11
+puts "Максимальный простой делитель числа: #{max_prostoi_del(number)}"
