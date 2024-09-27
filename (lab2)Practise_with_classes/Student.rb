29b62d696667bb09405b9e37513daac59dea77f3
unknown <danilreshetka@gmail.com>
2024-09-27 17:11:52 +0300

    (lab2) task 1 n 8. added regex to other fields

--- (lab2)Practise_with_classes/Student.rb
+++ (lab2)Practise_with_classes/Student.rb
@@ -2,25 +2,69 @@ class Student
   attr_accessor :last_name, :first_name, :patronymic_name, :id, :phone, :telegram, :email, :git
   
   def initialize(last_name, first_name, patronymic_name, id: nil, phone: nil, telegram: nil, email: nil, git: nil)
-    @last_name = last_name
-    @first_name = first_name
-    @patronymic_name = patronymic_name
-    @id = id
+    self.last_name = last_name
+    self.first_name = first_name
+    self.patronymic_name = patronymic_name
+    self.id = id
     self.phone = phone
-    @telegram = telegram
-    @email = email
-    @git = git
+    self.telegram = telegram
+    self.email = email
+    self.git = git
+
   end
 
-  def to_s
-    "ID: #{@id},
-    Фамилия: #{@last_name},
-    Имя: #{@first_name},
-    Отчество: #{@patronymic_name}, 
-    Телефон: #{@phone || 'не указан'},
-    Телеграм: #{@telegram || 'не указан'}, 
-    Почта: #{@email || 'не указана'},
-    Гит: #{@git || 'не указан'}"
+
+
+  def last_name=(last_name)
+    if name_regex_valid?(last_name)
+      @last_name = last_name
+    else
+      raise ArgumentError, "Фамилия должна содержать только буквы"
+    end
+  end
+
+  def first_name=(first_name)
+    if name_regex_valid?(first_name)
+      @first_name = first_name
+    else
+      raise ArgumentError, "Имя должно содержать только буквы"
+    end
+  end
+
+  def patronymic_name=(patronymic_name)
+    if name_regex_valid?(patronymic_name)
+      @patronymic_name = patronymic_name
+    else
+      raise ArgumentError, "Отчество должно содержать только буквы"
+    end
+  end
+
+  def name_regex_valid?(name)
+    name.match?(/\A[А-Яа-яЁёA-Za-z]+\z/)
+  end
+
+  def id=(id)
+    if id.is_a?(Integer)
+      @id = id
+    else
+      raise ArgumentError, "ID должен быть числом"
+    end
+  end
+
+  def telegram=(telegram)
+    if telegram.nil? || telegram.match?(/\A@[\w\d_]+\z/)
+      @telegram = telegram
+    else
+      raise ArgumentError, "Некорректный формат Telegram: #{telegram}"
+    end
+  end
+
+  def email=(email)
+    if email.nil? || email.match?(/\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i)
+      @email = email
+    else
+      raise ArgumentError, "Некорректный формат email: #{email}"
+    end
   end
 
   def phone=(phone)
@@ -31,8 +75,38 @@ class Student
     end
   end
 
+  def git=(git)
+    if git.match?(/^[a-zA-Z][a-zA-Z0-9-]{0,38}$/)
+      @git = git
+    else 
+      raise ArgumentError, "Некорректный формат Git: #{git}"
+    end
+  end
+
   def self.phone_regex_valid?(phone)
     phone.match?(/^\+?[1-9][0-9]{7,14}$/)
   end
+
+  def git_valid?
+    !git.nil?
+  end
+
+
+  def contact_valid?
+    !phone.nil? || !telegram.nil? || !email.nil?
+  end
+
+
+
   
+  def to_s
+    "ID: #{@id},
+    Фамилия: #{@last_name},
+    Имя: #{@first_name},
+    Отчество: #{@patronymic_name}, 
+    Телефон: #{@phone || 'не указан'},
+    Телеграм: #{@telegram || 'не указан'}, 
+    Почта: #{@email || 'не указана'},
+    Гит: #{@git || 'не указан'}"
+  end
 end
