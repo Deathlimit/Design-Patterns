458f20fb0faa0c538e20781ca0b6f8901c587561
Deathlimit <101141306+Deathlimit@users.noreply.github.com>
2024-10-25 19:33:46 +0300

    (lab3 task 1 n 2) added min element index finder

---
+++ Blocks_collections_etc/main.rb
@@ -0,0 +1,39 @@
+def find_min_index(arr)
+    arr.index(arr.min)
+end
+  
+puts "Выберите способ ввода данных:\n1. Ввести с клавиатуры\n2. Прочитать из файла"
+input_choice = gets.chomp
+  
+case input_choice
+    when "1"
+        puts "Введите целые числа через пробел:"
+        arr = gets.chomp.split.map(&:to_i)
+    when "2"
+        begin
+        file = File.open('C:\ruby\Blocks_collections_etc\input.txt', "r")
+        arr = file.read.split.map(&:to_i)
+        puts "Файл подключен успешно!"
+        rescue Errno::ENONET => error
+        puts "Обнаружена ошибка при работе с файлами: #{error}"
+        exit
+        ensure
+        file.close if file
+        end
+    else
+        puts "Некорректный выбор. Завершение программы."
+        exit
+end
+  
+puts "Выберите задачу:\n1. Найти индекс минимального элемента"
+method_choice = gets.chomp
+puts
+puts arr
+case method_choice
+    when "1"
+        puts "Индекс минимального элемента: #{find_min_index(arr)}"
+    else
+        puts "Некорректный выбор задачи."
+    end
+
+  
