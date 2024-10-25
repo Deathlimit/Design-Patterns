b0d0eb74c8f4d6a1cb9ddf5cd69d9d14c77a0e5d
Deathlimit <101141306+Deathlimit@users.noreply.github.com>
2024-10-26 00:33:30 +0300

    (lab3 task 1 n 50) added unique list symmetrical diff

--- Blocks_collections_etc/main.rb
+++ Blocks_collections_etc/main.rb
@@ -1,3 +1,9 @@
+def symm_diff(l1, l2)
+    uniq_l1 = l1.select { |element| l1.count(element) == 1 }
+    uniq_l2 = l2.select { |element| l2.count(element) == 1 }
+    (uniq_l1 | uniq_l2) - (uniq_l1 & uniq_l2)
+end
+
 def count_elements_between(arr)
     min_value = arr.min
     first_min = arr.index(min_value)
@@ -15,7 +21,8 @@ end
   
 puts "Выберите способ ввода данных:
                     \n1. Ввести с клавиатуры
-                    \n2. Прочитать из файла"
+                    \n2. Прочитать из файла строку
+                    \n3. Прочитать из файла списки"
 input_choice = gets.chomp
   
 case input_choice
@@ -33,6 +40,18 @@ case input_choice
         ensure
         file.close if file
         end
+    when "3"
+        begin
+        file = File.open('C:\ruby\Blocks_collections_etc\input_list.txt', "r")
+        l1 = file.read.split.map(&:to_i)
+        l2 = file.read.split.map(&:to_i)
+        puts "Файл подключен успешно!"
+        rescue Errno::ENONET => error
+        puts "Обнаружена ошибка при работе с файлами: #{error}"
+        exit
+        ensure
+        file.close if file
+        end
     else
         puts "Некорректный выбор. Завершение программы."
         exit
@@ -41,7 +60,9 @@ end
 puts "Выберите задачу:
         \n1. Найти индекс минимального элемента
         \n2. Найти количество элементов в заданном интервале
-        \n3. Найти количество элементов между первым и последним минимальным"
+        \n3. Найти количество элементов между первым и последним минимальным
+        \n4. ???
+        \n5. Построить новый список, состоящий из элементов, встречающихся только в одном из этих списков и не повторяющихся в них"
 method_choice = gets.chomp
 puts
 puts arr
@@ -58,6 +79,11 @@ case method_choice
     when "3"
         elements_count = count_elements_between(arr) { |first_min, last_min| (last_min - first_min - 1) }
         puts "Количество элементов между первым и последним минимальным значением: #{elements_count}"
+    when "4"
+        puts "???"
+    when "5"
+        symm_diff_result = symm_diff(l1, l2)
+        puts "Симметрическая разность списков: #{symm_diff_result}"
     else
         puts "Некорректный выбор задачи."
     end
