f5f26abf6aa46ea059dc7f90ca26da8e72383a2b
Deathlimit <101141306+Deathlimit@users.noreply.github.com>
2025-01-05 15:26:21 +0300

    (lab 5 task 0 n 7-8) aded crud buttons and filters reset

--- Students/MVC/student_list_view.rb
+++ Students/MVC/student_list_view.rb
@@ -12,8 +12,11 @@ class Student_list_view < FXVerticalFrame
   def initialize(parent)
     super(parent, opts: LAYOUT_FILL)
 
+    self.filters = {}
+
     setup_filtering_area
     setup_table_area
+    setup_control_buttons_area
   end
 
 
@@ -21,15 +24,25 @@ class Student_list_view < FXVerticalFrame
     filtering_area = FXVerticalFrame.new(self, opts: LAYOUT_FILL_X | LAYOUT_SIDE_TOP)
     FXLabel.new(filtering_area, "Фильтрация")
 
+    name_text_field = nil
     FXHorizontalFrame.new(filtering_area, opts: LAYOUT_FILL_X) do |frame|
       FXLabel.new(frame, "Фамилия и инициалы:")
       FXTextField.new(frame, 20, opts: TEXTFIELD_NORMAL)
+      name_text_field = FXTextField.new(frame, 20, opts: TEXTFIELD_NORMAL)
     end
+
+    self.filters['name'] = { text_field: name_text_field }
 
     add_filtering_row(filtering_area, "Git:")
     add_filtering_row(filtering_area, "Email:")
     add_filtering_row(filtering_area, "Телефон:")
     add_filtering_row(filtering_area, "Telegram:")
+
+    FXButton.new(filtering_area, "Сбросить", opts: BUTTON_NORMAL).connect(SEL_COMMAND) do
+      reset_filters
+    end
+
+
   end
 
 
@@ -45,6 +58,8 @@ class Student_list_view < FXVerticalFrame
 
       text_field = FXTextField.new(frame, 15, opts: TEXTFIELD_NORMAL)
       text_field.enabled = false
+
+      self.filters[label] = { combo_box: combo_box, text_field: text_field }
 
       combo_box.connect(SEL_COMMAND) do
         text_field.enabled = (combo_box.currentItem == 1)
@@ -74,19 +89,36 @@ class Student_list_view < FXVerticalFrame
 
     end
 
+    def setup_control_buttons_area
+      button_area = FXHorizontalFrame.new(self, opts: LAYOUT_FILL_X | PACK_UNIFORM_WIDTH)
+      self.add_button = FXButton.new(button_area, "Добавить", opts: BUTTON_NORMAL)
+      self.edit_button = FXButton.new(button_area, "Изменить", opts: BUTTON_NORMAL)
+      self.delete_button = FXButton.new(button_area, "Удалить", opts: BUTTON_NORMAL)
+      self.update_button = FXButton.new(button_area, "Обновить", opts: BUTTON_NORMAL)
+
+      self.add_button.connect(SEL_COMMAND) { on_add }
+      self.update_button.connect(SEL_COMMAND) { on_update }
+      self.edit_button.connect(SEL_COMMAND) { on_edit }
+      self.delete_button.connect(SEL_COMMAND) { on_delete }
+      self.table.connect(SEL_SELECTED) { update_button_states }
+      self.table.connect(SEL_DESELECTED) { update_button_states }
+
+      update_button_states
+    end
+
     populate_table
 
   end
 
   private
   
-  attr_accessor :table, :data, :total_pages, :current_page, :current_page_label, :prev_button, :next_button, :sort_order
+  attr_accessor :table, :data, :total_pages, :current_page, :current_page_label, :prev_button, :next_button, :sort_order, :add_button, :update_button, :edit_button, :delete_button,
+  :filters
 
 
 
 
   def populate_table
-
     data_list = Data_list_student.new([
       Student.new(last_name: "Решетка", first_name: "Даниил", patronymic_name: "Владимирович", git: "test1", id: 0, telegram: "@testtt1", birth_date: "01.01.1995", email: "wysi727@gmail.com", phone: "88005553535"),
       Student.new(last_name: "Арешетка", first_name: "Баниил", patronymic_name: "Младимирович", git: "test2", id: 1, telegram: "@testtt2", birth_date: "02.01.1995", email: "wysi728@gmail.com", phone: "88005553536"),
@@ -187,6 +219,53 @@ class Student_list_view < FXVerticalFrame
     self.data = Data_table.new(all_rows)
     update_table
 
+  end
+
+  def get_selected_rows
+    selected_rows = []
+    (0...self.table.numRows).each do |row|
+      selected_rows << row if self.table.rowSelected?(row)
+    end
+    selected_rows
+  end
+
+  def update_button_states
+    selected_rows = get_selected_rows
+  
+    self.add_button.enabled = true
+    self.update_button.enabled = true
+  
+    case selected_rows.size
+    when 0
+      self.edit_button.enabled = false
+      self.delete_button.enabled = false
+    when 1
+      self.edit_button.enabled = true
+      self.delete_button.enabled = true
+    else
+      self.edit_button.enabled = false
+      self.delete_button.enabled = true
+    end
+  end
+
+  def on_add
+  end
+  
+  def on_update
+  end
+  
+  def on_edit
+  end
+  
+  def on_delete
+  end
+
+  def reset_filters
+    self.filters.each_value do |field|
+      field[:combo].setCurrentItem(0) if !field[:combo].nil?
+      field[:text_field].text = ""
+    end
+    populate_table
   end
   
 end
